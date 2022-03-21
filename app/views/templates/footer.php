ecb6b8b724d1752fd62945b3928f6342761e7af0
Najib Fikri <najibfikri13@gmail.com>
2022-03-21 16:40:53 +0700

    Create footer.php

---
+++ app/views/templates/footer.php
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="<?= BASEURL;?>/js/bootstrap.js"></script>
+<script src="<?= BASEURL;?>/js/scriptt.js"></script>
+</body>
+</html>

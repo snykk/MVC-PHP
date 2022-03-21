bfb828854fe083fd08d050a5f3a680ed47c0a1f5
Najib Fikri <najibfikri13@gmail.com>
2022-03-21 16:43:28 +0700

    Create index.php

---
+++ app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <h1>About Me</h1>
+    <img class="rounded-circle" src="<?= BASEURL; ?>/img/kid.jpg" alt="kids life" width="250px">
+    <p>
+        Halo, nama saya <?= $data["nama"]; ?>, umur saya <?= $data["umur"]; ?> tahun, saya adalah seorang <?= $data["pekerjaan"]; ?>. 
+    </p>
+</div>
